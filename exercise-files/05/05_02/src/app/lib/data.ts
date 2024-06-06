b264414ba9da1d4f7dc33acc9cf0c2dfcad1ef5b
Sandra mada <sandramada@sandras-mbp.home>
2024-06-06 11:00:43 +0200

    05_02b

---
+++ exercise-files/05/05_02/src/app/lib/data.ts
@@ -0,0 +1,15 @@
+import { createClient } from '@vercel/postgres';
+
+export async function connectToDB() {
+  const client = createClient();
+  await client.connect();
+
+  try {
+    if (client) {
+      console.log('Connected to database');
+      return client;
+    }
+  } catch (error) {
+    console.error('Error connecting to database', error);
+  }
+}
