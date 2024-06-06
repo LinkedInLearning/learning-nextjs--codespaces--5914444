b8e9c1bedbf108369dfc7d5d9dfdf79887468039
Sandra mada <sandramada@sandras-mbp.home>
2024-06-06 14:17:35 +0200

    05_03b

---
+++ exercise-files/05/05_03/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3RDqtRqmapp9OKVs9knrz7t6BOzohJiWLJK1Cfp7STE=\"\n}"
---
+++ exercise-files/05/05_03/src/app/lib/data.ts
@@ -0,0 +1,17 @@
+import { createClient } from '@vercel/postgres';
+import { sql } from '@vercel/postgres';
+
+export async function connectToDB() {
+  const client = createClient();
+  await client.connect();
+
+  try {
+    if (client) {
+      console.log('Connected to database');
+      return client;
+    }
+  } catch (error) {
+    console.error('Error connecting to database', error);
+  }
+}
+
