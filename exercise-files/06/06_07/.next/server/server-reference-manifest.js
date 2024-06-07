dab40f2c0be6d65a1825977cf8dc2378cf98e664
Sandra mada <sandramada@sandras-mbp.home>
2024-06-07 20:13:44 +0200

    06_07b

---
+++ exercise-files/06/06_07/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"iax82sT92UMZmuTPz9DNgaAYawAFP1mMd7F2U0JYrYQ=\"\n}"
---
+++ exercise-files/06/06_07/src/app/blog/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <>
+      <h1 className="text-purple-800">About</h1>
+      <p>Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.</p>
+    </>)
+
+}
