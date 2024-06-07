289f838a28fc5cf5795d7cf3484e88fa2bf73f65
Sandra mada <sandramada@sandras-mbp.home>
2024-06-07 19:36:55 +0200

    06_06

--- exercise-files/06/06_06/src/app/ui/components/posts/Post.tsx
+++ exercise-files/06/06_06/src/app/ui/components/posts/Post.tsx
@@ -1,9 +1,10 @@
 import React, { JSX } from 'react';
+import Link from 'next/link'
 
 export default function Component({ id, title, content, date }: { id: string, title: string, content: string, date: string }) {
     return (
         <div key={id} className="border border-gray-200 p-4 my-4">
-            <h2>{title}</h2>
+            <Link href={`/blog/post/${id}`}><h2 className="font-semibold text-lg">{title}</h2></Link>
             <p className="text-gray-500">{date}</p>
             <p>{content}</p>
         </div>
